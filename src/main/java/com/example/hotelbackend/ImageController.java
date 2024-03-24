603e1371d3de7455980d8c297ed60197b83f956b
b4rtm <bartek.matusiak12@gmail.com>
2024-03-24 21:14:33 +0100

    moved images to backend

---
+++ src/main/java/com/example/hotelbackend/ImageController.java
@@ -0,0 +1,32 @@
+package com.example.hotelbackend;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.nio.file.Files;
+
+@RestController
+@RequestMapping("/images")
+public class ImageController {
+
+    @GetMapping("/{imageName}")
+    public ResponseEntity<byte[]> getImage(@PathVariable String imageName) {
+        try {
+            Resource resource = new ClassPathResource("static/images/" + imageName);
+            byte[] imageBytes = Files.readAllBytes(resource.getFile().toPath());
+            return ResponseEntity.ok()
+                    .contentType(MediaType.IMAGE_JPEG)
+                    .body(imageBytes);
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+}
